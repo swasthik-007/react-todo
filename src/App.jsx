dc4b19320e0e267474dee49ef6e90b6cc693fc01
swashik-007 <mohanty.swastik4@gmail.com>
2024-05-06 18:59:15 +0530

    first stage is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-
+import TodoList from './components/TodoList/TodoList'
 import './App.css'
 
 function App() {
  
+const todos=[
+  {id:1, text:'todo1',isFinished:true},
+  {id:2, text:'todo2',isFinished:false},
+]  
 
   return (
     <>
-      Todos
+      <TodoList todos={todos}/>
     </>
   )
 }
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,14 @@
+
+
+function Todo({text,isFinished}){
+    return(
+        <div>
+            <input type="checkbox" checked={isFinished} />
+            <span>  {text}  </span>
+            <button> Edit   </button>
+            <button> Delete </button> 
+        </div>
+    )
+    
+    }
+    export default Todo;
